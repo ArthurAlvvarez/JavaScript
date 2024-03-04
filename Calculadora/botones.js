2a6bdcc23b020a1d79cfc7b752a0ad93d08de33e
ArthurAlvvarez <arturo.alvarez.gomez@colegiolitterator.com>
2024-03-04 17:45:43 +0100

    Falta M+

--- Calculadora/botones.js
+++ Calculadora/botones.js
@@ -1,3 +1,42 @@
-function porcentaje(){
-       let resultado = document.calculadora.ans.value / 100;
+ function porcentaje(){
+            let resultado = 1;
+            let num = document.calculadora.ans.value;
+            resultado = num / 100;
+            document.calculadora.ans.value = resultado;
+        }
+        function factorial(){
+            let num = document.calculadora.ans.value;
+            resultado = 1;
+            for(i = num; i > 0;i--){
+                resultado *= i;
+            }
+            document.calculadora.ans.value = resultado;
+        }
+         function x(){
+            let num = document.calculadora.ans.value;
+            let resultado = 1;
+            resultado = Math.pow(10,num);
+            document.calculadora.ans.value = resultado;
+        }
+        function deleteChar() {
+            operacion = document.calculadora.ans.value;
+            document.calculadora.ans.value = operacion.substring(0, operacion.length-1);
+        }
+        function diez(){
+            let num = document.calculadora.ans.value;
+            let resultado = 1;
+            resultado = Math.pow(num,10);
+            document.calculadora.ans.value = resultado;
+        }
+        function cubica(){
+            let num = document.calculadora.ans.value;
+            let resultado = 1;
+            resultado = Math.pow(num, 1 / 3);
+            document.calculadora.ans.value = resultado;
+        }
+        function guardar(){
+            var numero = eval(document.calculadora.ans.value);
+        }
+        function m(){
+            document.calculadora.ans.value = numero;
         }
